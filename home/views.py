356dad210c921433bbcd3b734119cec909f50a62
YashKrSaini <ysaini_be18@thapar.edu>
2021-03-22 13:29:02 +0530

    second update

--- home/views.py
+++ home/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 
 # Create your views here.
-def home(request):
+def index(request):
     return render(request,'index.html')
 
 def login(request):
@@ -9,3 +9,4 @@ def login(request):
 
 def newlog(request):
     return render(request,'newlog.html')
+
